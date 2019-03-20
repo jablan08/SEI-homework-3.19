21620a364c4dd9145e35eb79a89841ebcd2b8f40
Jablan <joshuaablan@gmail.com>
2019-03-19 19:38:09 -0700

    Commit 9 - Created loops.js file

---
+++ loop-questions/app.js
@@ -0,0 +1,35 @@
+for (let i = 0; i <= 99; i++) {
+    console.log(i);
+}
+
+for (let i = 99; i >= 0; i--) {
+    console.log(i);
+}
+
+for (let i = 298; i >= 1; i -= 3) {
+    console.log("This num is " + i);
+}
+
+for (let i = 0; i <= 98; i += 2) {
+    console.log(i);
+}
+
+for (let i = 98; i >= 0; i--) {
+    if (i % 2 !== 0) {
+    console.log(i);
+    }
+}
+
+for (let i = 49; i <= 72; i++) {
+    console.log("This num is " + i);
+}
+
+for (let i = 81; i >= 26; i--) {
+    console.log("This new num " + i);
+}
+
+for (let i = 1; i <= 91; i++) {
+    if (i % 3 == 0) {
+    console.log("This number " + i + " is a multiple of 3.");
+    }
+}
